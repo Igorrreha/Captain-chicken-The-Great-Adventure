6bd695939f171b5e3fb7e1b68aa5ddc16384e9c7
Igorrreha <igorecha.9999@gmail.com>
2020-03-30 08:35:26 +0600

    Готовый вариант главного героя, шаблон механизмов

--- CaptainChicken.gd
+++ CaptainChicken.gd
@@ -12,6 +12,16 @@ export var runAcc = 10
 export var runDec = 10
 export var ladderClimbSpeed = 30
 
+var berserkerModeOn = false
+
+var maxLandingSpeed = 300
+
+export var hp = 100
+
+
+signal hero_dead
+
+
 func _ready():
 	state = set_state()
 
@@ -36,6 +46,7 @@ func _process(delta):
 					if area.is_in_group("ladder"):
 						moveVec = Vector2(0, 0)
 						state = STATE.ON_LADDER
+						$AnimationPlayer.play("LadderClimb")
 		else:
 			
 			# проверяем, пересекает ли персонаж лестницу
@@ -56,52 +67,169 @@ func _process(delta):
 			if (isLadderOverlapped and is_on_floor() and not Input.is_action_pressed("up")) or not isLadderOverlapped:
 				state = STATE.IDLE
 	
-	if state != STATE.ON_LADDER and state != STATE.IN_ACTION:
-		# бег
-		if inpVec.x == 0:
-			if moveVec.x != 0:
-				moveVec.x += (runDec if moveVec.x < 0 else -runDec)
-		elif abs(moveVec.x + (inpVec.x * runAcc)) > runSpeed:
-			moveVec.x = runSpeed if inpVec.x > 0 else -runSpeed
-		else: 
-			moveVec.x += inpVec.x * runAcc
+	
+	# отражение по x
+	if state != STATE.IN_ACTION:
+		if inpVec.x > 0:
+			$Sprite.set_flip_h(false)
+		elif inpVec.x < 0:
+			$Sprite.set_flip_h(true)
+	
+	
+	# перемещение в режиме берсерка
+	if berserkerModeOn:
+		state = STATE.RUN
+		
+		moveVec.x = runSpeed * (-1.75 if $Sprite.is_flipped_h() else 1.75)
 		
 		# гравитация
 		if not is_on_floor():
 			moveVec.y += GRAV
 	
+	# обычное перемещение
+	else:
+		if state != STATE.ON_LADDER and state != STATE.IN_ACTION:
+			# бег
+			if inpVec.x == 0:
+				if moveVec.x != 0:
+					moveVec.x += (runDec if moveVec.x < 0 else -runDec)
+			elif abs(moveVec.x + (inpVec.x * runAcc)) > runSpeed:
+				moveVec.x = runSpeed if inpVec.x > 0 else -runSpeed
+			else: 
+				moveVec.x += inpVec.x * runAcc
+			
+			# гравитация
+			if not is_on_floor():
+				moveVec.y += GRAV
+	
 	move_and_slide(moveVec, Vector2(0, -1))
 	
 	set_state()
+	set_animation()
+
 
 func _input(event):
 	
 	# прыжок / спрыгивание с лестницы
 	if event.is_action_pressed("jump"):
-		if is_on_floor() or state == STATE.ON_LADDER:
+		if (is_on_floor() or state == STATE.ON_LADDER) and state != STATE.IN_ACTION:
 			moveVec.y -= jumpForce
 			
 			state = STATE.JUMP
 	
 	# действие
-	if event.is_action_pressed("action"):
-		var overlappingAreas = $ActionArea.get_overlapping_areas()
+	if event.is_action_pressed("action") and state != STATE.IN_ACTION  and state != STATE.ON_LADDER and not berserkerModeOn:
 		
+		var overlappingAreas = $ActionArea.get_overlapping_areas()
+		for area in overlappingAreas:
+			
+			if area.is_in_group("mecha"):
+				if area.isActive == area.defaultIsActive or area.isToogleable:
+					state = STATE.IN_ACTION
+					moveVec = Vector2(0, 0)
+					$Sprite.set_flip_h(true if area.scale.x > 0 else false)
+					
+					# включение анимации действия
+					var actionAnimationName = (area.deactivateAnimation if area.isActive else area.activateAnimation)
+					$AnimationPlayer.play("action_" + actionAnimationName)
+					
+					# таймер активации/деактивации механизма
+					var timeToActionStart = get_time_to_action_start(actionAnimationName)
+					var timer = get_node(str(area.get_path()) + "/ToogleTimer")
+					timer.set_wait_time(timeToActionStart)
+					timer.start()
+					
+					# позиционирование в точку действия
+					var newPos = area.position + get_node(str(area.get_path()) + "/ActionPos").position * area.scale
+					var tween = $Tween
+					tween.interpolate_property(self, "position",
+						position, newPos, timeToActionStart,
+						Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+					tween.start()
+					
+					break
 
+
+# изменение состояния исходя из вектора движения и текущего положения в пространстве
 func set_state():
 	if state != STATE.ON_LADDER and state != STATE.IN_ACTION:
 		if is_on_floor():
 			if moveVec.y < 0:
-				state = STATE.JUMPING
+				jump_start()
 			elif moveVec.y > GRAV:
-				state = STATE.LANDING
+				landing()
+				if moveVec.y > maxLandingSpeed:
+					hit(100)
+				
 				moveVec.y = 0
-			elif moveVec.x == 0:
+				
+			if moveVec.x == 0:
 				state = STATE.IDLE
 			else:
 				state = STATE.RUN
 		else: 
 			if moveVec.y > 0:
-				state = STATE.JUMP
-			else:
 				state = STATE.FALL
+			else:
+				state = STATE.JUMP
+
+
+# начало прыжка
+func jump_start():
+	pass
+# приземление
+func landing():
+	pass
+
+# проигрывание анимации
+func set_animation():
+	if state == STATE.IDLE:
+		$AnimationPlayer.play("Idle")
+	elif state == STATE.RUN:
+		$AnimationPlayer.play("Run")
+	elif state == STATE.JUMP:
+		$AnimationPlayer.play("Jump")
+	elif state == STATE.FALL:
+		$AnimationPlayer.play("Fall")
+	elif state == STATE.ON_LADDER:
+		if moveVec.y == 0:
+			$AnimationPlayer.stop()
+			$AnimationPlayer.current_animation = "LadderClimb"
+		elif moveVec.y < 0:
+			$AnimationPlayer.play("LadderClimb")
+		elif moveVec.y > 0:
+			$AnimationPlayer.play_backwards("LadderClimb")
+
+
+# получить промежуток времени от момента начала анимации взаимодействия с механизмом до момента применения этого действия
+func get_time_to_action_start(animationName):
+	match animationName:
+		"buttonPress": 
+			return 0.4
+		"flyKick":
+			return 0.8
+		"":
+			return 0.0
+
+
+# обработка окончания анимации
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name.find("action_") != -1:
+		$AnimationPlayer.play("Idle")
+		state = STATE.IDLE
+
+
+# получение урона
+func hit(damage):
+	hp -= damage
+	
+	if hp <= 0:
+		berserkerModeOn = true
+		$BerserkerModeTimer.start()
+
+
+# смерть
+func death():
+	queue_free()
+	
+	emit_signal("hero_dead")
--- Main.gd
+++ Main.gd
@@ -3,9 +3,19 @@ extends Node2D
 var Console
 var Hero
 
+
 func _ready():
 	Console = $Conosle
 	Hero = $Hero
+	
+	Hero.connect("hero_dead", self, "restart")
+
 
 func _process(delta):
-	Console.text = str(Hero.moveVec) + " " + str(Hero.state) + " " + str(Hero.is_on_floor())
+	
+	if has_node("Hero"):
+		Console.text = str(Hero.moveVec) + " " + str(Hero.state) + " " + str(Hero.is_on_floor()) + " " + str(Hero.hp)
+
+
+func restart():
+	get_tree().reload_current_scene()
