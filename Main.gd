daaf082d7a7bb26f63584499f753ec54d7bfdf64
Igorrreha <igorecha.9999@gmail.com>
2020-04-04 13:49:18 +0600

    Добавлен объект Grabber

--- Main.gd
+++ Main.gd
@@ -13,9 +13,11 @@ func _ready():
 
 func _process(delta):
 	
+	# отображение информации в консоли (тест)
 	if has_node("Hero"):
 		Console.text = str(Hero.moveVec) + " " + str(Hero.state) + " " + str(Hero.is_on_floor()) + " " + str(Hero.hp)
 
 
+# перезапуск уровня
 func restart():
 	get_tree().reload_current_scene()
